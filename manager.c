84168c2d08e34d7e15d2b1419af4da2953183e6b
alsco1234 <22000080@handong.edu>
2021-05-06 17:09:59 +0900

    make manager.c

---
+++ manager.c
@@ -0,0 +1,22 @@
+#include "manager.h"
+
+void saveData(Clothes *c, int count){
+
+}
+
+int loadData(Clothes *c){
+
+}
+
+int searchName(Clothes *c, int count){
+
+}
+int searchPrice(Clothes *c, int count){
+
+}
+int searchSize(Clothes *c, int count){
+
+}
+int searchNum_stars(Clothes *c, int count){
+
+}
